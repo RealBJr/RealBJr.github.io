748a26006230b4a6f4f73292b72f947c7ba42a81
Junior Boni <juniorboni003@gmail.com>
2025-01-18 18:31:20 -0500

    Project Setup

---
+++ junior-portfolio/app/page.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <div id="About">About</div>
+      <div id="Skills">Skills</div>
+      <div id="Experience">Experience</div>
+      <div id="Projects">Projects</div>
+      <div id="Education">Education</div>
+    </>
+  );
+}
